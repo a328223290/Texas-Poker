d760e575107fe633508ad39664db3b2c862dd044
a328223290 <gexinkai2017@163.com>
2026-03-02 21:55:09 +0800

    fix: 补充springcloud负载均衡包，解决poker-gateway无法订阅其他服务的问题

--- poker-backend/poker-user/src/main/java/com/poker/user/UserServiceApplication.java
+++ poker-backend/poker-user/src/main/java/com/poker/user/UserServiceApplication.java
@@ -1,11 +1,13 @@
 package com.poker.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.poker.user", "com.poker.common"})
 @EnableDiscoveryClient
+@MapperScan("com.poker.user.mapper")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
